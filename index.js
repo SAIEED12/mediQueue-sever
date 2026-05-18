e4c600a483b67e4b7fe74b7ef0fc2c3dac611381
SAIEED12 <abusaieed0@gmail.com>
2026-05-18 11:40:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config()
+const app = express()
+app.use(cors())
+const port = process.env.PORT || 8000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("mediQueuedb")
+    const tutorsCollection = db.collection("tutors")
+
+    //All Data API
+    app.get('/tutors', async(req, res) =>{
+        const cursor = tutorsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    //Single Data API
+    app.get('/tutors/:tutorId', async(req, res) =>{
+        const {tutorId} = req.params
+        const query = {_id: new ObjectId(tutorId)}
+        const result = await tutorsCollection.findOne(query)
+        res.send(result)
+    })
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
